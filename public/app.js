cda772b4a3c29d01fa860a56832ec529c147a059
laportenier <lilly.portenier@gmail.com>
2020-01-23 20:49:12 -0500

    got data rendering on page

--- public/app.js
+++ public/app.js
@@ -1,3 +1,36 @@
 $.getJSON("/articles", function (data) {
-    
+    for (var i = 0; i < data.length; i++) {
+        $("#articles").append("<div class='card-body'> <h5  class='card-title' data-id='" + data[i]._id + "'>" + data[i].title + "</h5>");
+
+        $("#articles").append("<p class='card-text'>" + data[i].description + "</p>");
+
+        $("#articles").append("<p class='card-text'> Author: " + data[i].author + "</p>");
+
+        $("#articles").append("<a href='" + data[i].link + "' class='btn btn-primary'> Link to Article</a> </div>")
+    };
+});
+
+$(document).on("click", "#noteBtn", function (){
+
+    // $("#notes").empty();
+    var thisId = this.attr("data-id");
+    $.ajax({
+        method: "GET",
+        url: "/articles/" + thisId
+    }) .then(function (data){
+        // for (j = 0; j <data.length; j++){
+            $("#notes").append("<p>" + data[j].title + "</p>");
+            $("#notes").append("<input id='titleinput' name='title' >");
+            $("#notes").append("<textarea id='bodyinput' name='body'></textarea>");
+            $("#notes").append("<button data-id='" + data[j]._id + "' id='savenote'>Save Note</button>");
+
+            if (data[i].note){
+                $("#titleinput").val(data[0].note.title);
+                $("#bodyinput").val(data[0].note.body);
+
+            }
+        // };
+        
+    })
+
 })
--- server.js
+++ server.js
@@ -1,33 +1,93 @@
 // var http = require("http");
 var express = require("express");
-var mongojs = require("mongojs");
+var logger = require("morgan");
 var mongoose = require("mongoose");
+// var mongojs = require("mongojs");
+
 
 var axios = require("axios");
 var cheerio = require("cheerio");
 
-var databaseUrl = "scraper";
-var collections = ["scrapedData"];
+var db = require("./models");
 
+
+// var databaseUrl = "scraper";
+// var collections = ["scrapedData"];
+
+var PORT = 3000;
 
 var app = express();
 
 
-var PORT = 3000;
-
+app.use(logger("dev"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static("public"));
+
 
 mongoose.connect("mongodb://localhost/dbMongo", { useNewUrlParser: true });
-// function handleRequest(request, response) {
-//   response.end("It Works!! Path Hit: " + request.url);
-// }
 
-// var server = http.createServer(handleRequest);
 
-require("./routes/htmlRoutes")(app);
+app.get("/scrape", function (req, res) {
+  axios.get("https://www.nytimes.com/column/books-of-the-times").then(function (response) {
+    var $ = cheerio.load(response.data);
 
-app.listen(PORT, function() {
+    $("li.css-ye6x8s").each(function (i, element) {
+      var result = {};
 
+      result.title = $(this).find("a").find("h2").text();
+      result.description = $(this).find("a").find("p").text();
+      result.author = $(this).find("div.css-1nqbnmb").find("span").text();
+      result.link = "https://www.nytimes.com/" + $(this).find("div.css-1l4spti").find("a").attr("href");
+
+      db.Article.create(result)
+        .then(function (dbArticle) {
+          console.log(dbArticle);
+        })
+        .catch(function (err) {
+          console.log(err)
+        });
+    });
+    res.send("scrape Complete")
+  });
+});
+
+app.get("/articles", function (req, res) {
+  db.Article.find({})
+  .then(function (dbArticle) {
+    // console.log(dbArticle, "re josn")
+    res.json(dbArticle)
+    
+  }).catch(function (err) {
+    res.json(err);
+  });
+});
+
+app.get("/articles/:id", function (req, res) {
+  db.Article.find({ _id: req.params.id}).populate("note")
+  .then(function (dbArticle){
+    res.json(dbArticle);
+  }).catch (function (err) {
+    res.json(err);
+  });
+});
+
+app.post("/article/:id", function (req, res) {
+  db.Note.create(req.body)
+  .then(function (dbNote){
+    return db.Article.findOneAndUpdate({_id: mongojs.ObjectId(req.params.id)}, {$push: { note: dbNote._id }}, {new: true})
+  })
+  .then (function (dbArticle){
+    res.json(dbArticle);
+  })
+  .catch(function(err) {
+    res.json(err);
+  });
+});
+
+
+// require("./routes/htmlRoutes")(app);
+
+app.listen(PORT, function () {
   console.log("Server listening on: http://localhost:" + PORT);
 });
